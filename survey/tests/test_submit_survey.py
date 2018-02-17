cce74bb54d138bba5b1842843d61669bcbee5134
Jarno Luoma-Nirva <jln@iki.fi>
2018-02-17 15:22:40 +0200

    Calculate and show result after submitting answers

--- survey/tests/test_submit_survey.py
+++ survey/tests/test_submit_survey.py
@@ -7,4 +7,23 @@ class SubmitSurveyPageTest(TestCase):
     url_name = 'survey_submit'
 
     def test_reverse(self):
-        self.assertEqual(reverse(self.url_name, args=['1']), '/survey/1/submit/')
+        self.assertEqual(reverse(self.url_name, args=['ts']), '/survey/ts/submit/')
+
+    def test_uses_correct_template(self):
+        survey = models.Survey.objects.create(name="test_survey", title="Test survey")
+        response = self.client.post(reverse(self.url_name, args=[survey.name]), follow=True)
+        self.assertTemplateUsed(response, 'survey/result.html')
+
+    def test_submit_survey(self):
+        survey = models.Survey.objects.create(name="test_survey", title="Test survey")
+        q1 = models.Question.objects.create(survey=survey, title="Question 1")
+        response = self.client.post(reverse(self.url_name, args=[survey.name]),
+                                    data={'q-{}'.format(q1.id): '10'},
+                                    follow=True)
+        self.assertEqual(response.context['survey'], survey)
+        self.assertEqual(response.context['max_score'], 10)
+        self.assertEqual(response.context['score'], 10)
+        self.assertEqual(response.context['nscore'], 10)
+        self.assertEqual(response.context['pct'], 100.0)
+        self.assertEqual(response.context['npct'], 100.0)
+        self.assertEqual(len(response.context['answers']), 1)
--- survey/urls.py
+++ survey/urls.py
@@ -5,7 +5,7 @@ from . import survey
 
 
 urlpatterns = [
-    path('survey/<int:survey_id>/submit/', views.submit_survey, name='survey_submit'),
+    path('survey/<slug:survey_name>/submit/', views.submit_survey, name='survey_submit'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('', survey.SurveyView.as_view(), name='survey'),
 ]
