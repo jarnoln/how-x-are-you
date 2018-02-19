cdb7582fb2d44ba5a7797e3d483d1603b19bd2f5
Jarno Luoma-Nirva <jln@iki.fi>
2018-02-19 20:13:39 +0200

    Refactor:Submit survey page to own file

--- survey/urls.py
+++ survey/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from . import views
 from . import survey
-
+from .submit_survey import submit_survey
 
 urlpatterns = [
     path('survey/first/', survey.SurveyView.as_view(), name='survey_first'),  # Show the first (usually the only) survey
-    path('survey/<slug:survey_name>/submit/', views.submit_survey, name='survey_submit'),
+    path('survey/<slug:survey_name>/submit/', submit_survey, name='survey_submit'),
     path('survey/<slug:survey_name>/', survey.SurveyView.as_view(), name='survey_detail'),
     path('surveys/', survey.SurveyList.as_view(), name='survey_list'),
     path('home/', views.HomeView.as_view(), name='home'),
--- survey/views.py
+++ survey/views.py
@@ -1,58 +1,5 @@
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.shortcuts import get_object_or_404, render
-from survey import models
 
 
 class HomeView(TemplateView):
     template_name = 'survey/home.html'
-
-
-def submit_survey(request, survey_name):
-    survey = get_object_or_404(models.Survey, name=survey_name)
-    reference = models.Reference.objects.filter(survey=survey).first()
-    answer_list = []
-    score = 0
-    nscore = 0
-    for question in survey.question_set.all():
-        key = 'q-{}'.format(question.id)
-        answer_value = int(request.POST.get(key, '0'))
-        points = answer_value  # Value between -2 and +2 (0 being no comment)
-        npoints = answer_value + 2  # Value between 0 and 4 (2 being no comment)
-        answer_list.append({'question': question,
-                            'answer': answer_value,
-                            'points': points,
-                            'npoints': npoints
-                            })
-        score += points
-        nscore += npoints
-        # print('{}:{}'.format(question.title, answer_value))
-
-    max_score = survey.question_set.count() * 2
-    nmax_score = survey.question_set.count() * 4
-    pct = 0
-    npct = 0
-    if max_score > 0:
-        pct = float(score * 100) / max_score
-        npct = float(nscore * 100) / nmax_score
-
-    if reference:
-        feedback = reference.feedback(npct)
-    else:
-        feedback = None
-
-    context = {
-        'survey': survey,
-        'reference': reference,
-        'feedback': feedback,
-        'answers': answer_list,
-        'score': score,
-        'nscore': nscore,
-        'max_score': max_score,
-        'nmax_score': nmax_score,
-        'pct': pct,
-        'npct': npct
-    }
-    return render(request, 'survey/result.html', context)
-    # return HttpResponseRedirect(reverse('survey'))
